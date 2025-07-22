f35d91fdba1db74fecf785173ff993ba20528639
Hemavarna-S <hemavarnasundararajkumar@gmail.com>
2025-07-22 22:09:43 +0530

    Server-api change6

--- hexnex-frontend/src/utils/api.js
+++ hexnex-frontend/src/utils/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'https://hexnex-t1j2.onrender.com',
+const axiosInstance = axios.create({
+  baseURL: 'https://hexnex-t1j2.onrender.com/api', // 👈 IMPORTANT: add /api because your backend uses app.use('/api')
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export default instance;
+export default axiosInstance;

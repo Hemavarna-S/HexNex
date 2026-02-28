afe1a7572739b5aae8e5419a97d7e64f50c7407f
Hemavarna-S <hemavarnasundararajkumar@gmail.com>
2026-02-28 10:15:56 +0530

    This is pushing in git -> on render

--- hexnex-frontend/src/utils/api.js
+++ hexnex-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://hexnex-t1j2.onrender.com/', // 👈 IMPORTANT: add /api because your backend uses app.use('/api')
+  baseURL: 'https://hexnex-1.onrender.com', // 👈 IMPORTANT: add /api because your backend uses app.use('/api')
   headers: {
     'Content-Type': 'application/json',
   },
